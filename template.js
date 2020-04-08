80097e043597ffcc336aa5ca13e0a4ed94d71e4c
A Faraday (post baby) <ajfaraday@edge-ka.com>
2020-04-08 16:45:25 +0100

    Bare bones

---
+++ template.js
@@ -0,0 +1,15 @@
+Teams.template = [
+  function(api, my_storage, shared_storage) {
+    // NW hunter
+  },
+  function(api, my_storage, shared_storage) {
+    // NE hunter
+  },
+  function(api, my_storage, shared_storage) {
+    // SE hunter
+  },
+  function(api, my_storage, shared_storage) {
+    // SW hunter
+  }
+]
+
