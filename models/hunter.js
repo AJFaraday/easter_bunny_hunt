0562d1b2de395574cd594b964984a086d55f620b
Andrew Faraday <AJFaraday@users.noreply.github.com>
2020-04-08 20:48:33 +0100

    Update hunter.js

--- models/hunter.js
+++ models/hunter.js
@@ -17,10 +17,48 @@ class Kid {
       this.game.storage
     );
     this.move();
+    this.check_collisions();
+  }
+
+  check_collisions() {
+
   }
 
   move() {
-    
+    switch(this.direction) {
+      case n:
+        this.y -= 1;
+        break;
+      case e:
+        this.x += 1;
+        break;
+      case s:
+        this.y += 1;
+        break;
+      case w:
+        this.x -= 1;
+    }
+  }
+
+  on_egg() {
+    var kid = this;
+    return(
+      this.game.eggs.find(
+        function(egg) {
+          return(
+            egg.x == this.x &&
+            egg.y == this.y
+          );
+        }
+      );
+    )
+  }
+
+  on_bunny() {
+    return (
+      this.game.bunny.x == this.x &&
+      this.game.bunny.y == this.y
+    );
   }
 
 }
