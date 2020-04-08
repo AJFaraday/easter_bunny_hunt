40b2f3dda4273e91ecb8b74a281a16d749da9a24
Andrew Faraday <AJFaraday@users.noreply.github.com>
2020-04-08 21:46:31 +0100

    Update api.js

--- models/api.js
+++ models/api.js
@@ -2,7 +2,7 @@ clsss Api {
 
   constructor(game, kid) {
     this.game = game;
-    this.kid = kid;
+    this.source_kid = kid;
     this.storage = {};
   }
 
@@ -13,6 +13,66 @@ clsss Api {
   shared_storage() {
     this.game.storage;
   }
+
+  north() {
+    this.kid.direction = 'n';
+  }
+
+  east() {
+    this.kid.direction = 'e'
+  }
+
+  south() {
+    this.kid.direction = 's';
+  }
+
+  west() {
+    this.kid.direction = 'w';
+  }
+
+  turn() {
+    return this.game.turn;
+  } 
+
+  eggs() {
+    this.game.eggs.map(
+      function(egg) {
+        return {x: egg.x, y: egg.y});
+      }
+    );
+  }
   
+  bunny() {
+    return {
+      x: this.game.bunny.x,
+      y: this.game.bunny.y,
+      eggs_remaining: this.game.bunny.eggs_remaining
+    }
+  }
+
+  kids() {
+    var api = this;
+    this.game.kids.map(
+      function(kid) {
+        return(
+          {
+            x: kid.x,
+            y: kid.y,
+            me: (kid == api.source_kid)
+          }
+        );
+      }
+    ):
+  }
+
+  kid() {
+    return(
+      {
+        x: this.source_kid.x,
+        y: this.source_kid.y,
+        me: true
+      }
+    );
+  }
 
 }
